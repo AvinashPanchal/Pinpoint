a2961247a645a6258fde1be9919fa06f21ad11f6
AvinashPanchal <er.avinashpanchal@gmail.com>
2016-12-17 17:55:27 +0530

    Add files via upload

---
+++ Setup.php
@@ -0,0 +1,17 @@
+<?php
+/*
+* This file is part of the WeAgenda project.
+*
+* (c) Akeem <akeem.philbert@wepala.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Test;
+
+
+class Setup extends \PHPUnit_Framework_TestCase
+{
+
+}
